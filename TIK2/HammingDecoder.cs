7f6e06e7795f4bc68051a4119f8d90907d8896e7
BardiTheWeird <48732117+BardiTheWeird@users.noreply.github.com>
2021-05-03 01:30:17 +0300

    returned error handling for hamming stuff

--- TIK2/HammingDecoder.cs
+++ TIK2/HammingDecoder.cs
@@ -24,83 +24,83 @@ namespace TIK2
         {
             BitReader br = null;
             BitWriter bw = null;
-            //try
-            //{
-            var logSb = new StringBuilder();
-            _sw.Start();
-            br = new BitReader(filepathIn);
-            bw = new BitWriter(filepathOut);
-
-            br.ReadBits(16, out var overflownLenBuffer);
-            var overflownLen = HammingCodes.DecodeHamming(overflownLenBuffer).Item1.ByteBuffer[0];
-            if (overflownLen > 0)
+            try
             {
-                br.ReadBits(blockSize, out var overflownPadded);
-                var paddedInfo = HammingCodes.DecodeHamming(overflownPadded);
-                logSb.AppendLine(paddedInfo.Item3);
+                var logSb = new StringBuilder();
+                _sw.Start();
+                br = new BitReader(filepathIn);
+                bw = new BitWriter(filepathOut);
+
+                br.ReadBits(16, out var overflownLenBuffer);
+                var overflownLen = HammingCodes.DecodeHamming(overflownLenBuffer).Item1.ByteBuffer[0];
+                if (overflownLen > 0)
+                {
+                    br.ReadBits(blockSize, out var overflownPadded);
+                    var paddedInfo = HammingCodes.DecodeHamming(overflownPadded);
+                    logSb.AppendLine(paddedInfo.Item3);
                     
-                var overflownInfo = new BitBuffer();
-                for (int i = 0; i < overflownLen; i++)
-                    overflownInfo.AppendBit(paddedInfo.Item1[i]);
+                    var overflownInfo = new BitBuffer();
+                    for (int i = 0; i < overflownLen; i++)
+                        overflownInfo.AppendBit(paddedInfo.Item1[i]);
 
-                bw.WriteBuffer(overflownInfo);
+                    bw.WriteBuffer(overflownInfo);
+                }
+
+                var previousPercentage = -1;
+
+                while (br.BitsRead < br.FileLength * 8 - blockSize)
+                {
+                    if (token.IsCancellationRequested)
+                    {
+                        br.StopReading();
+                        bw.StopWriting();
+                        _sw.Reset();
+                        Log = "";
+                        return string.Empty;
+                    }
+
+                    br.ReadBits(blockSize, out var encodedInfo);
+                    var decoding = HammingCodes.DecodeHamming(encodedInfo);
+                    bw.WriteBuffer(decoding.Item1);
+                    logSb.AppendLine(decoding.Item3);
+
+                    var percentage = (int)(Math.Round(br.BytesRead / (float)br.FileLength, 2) * 100);
+                    if (percentage > previousPercentage)
+                    {
+                        previousPercentage = percentage;
+                        Log = $"Huffman-decoding... {percentage * 1}%;\tTime elapsed: {_sw.ElapsedMilliseconds / 1000f:.00}s";
+                    }
+                }
+                bw.WriteTheRestOfTheBuffer();
+                bw.StopWriting();
+                br.StopReading();
+
+                File.WriteAllText(_decoderLogPath, logSb.ToString());
+
+                _sw.Stop();
+                Log = "";
+                var res = $"Finished decoding. Decoded file:{Path.GetFileName(filepathOut)}.\n" +
+                    $"\tTime elapsed: {_sw.ElapsedMilliseconds / 1000f:.00}s";
+                _sw.Reset();
+                return res;
             }
-
-            var previousPercentage = -1;
-
-            while (br.BitsRead < br.FileLength * 8 - blockSize)
+            catch (Exception e)
             {
-                if (token.IsCancellationRequested)
-                {
-                    br.StopReading();
-                    bw.StopWriting();
-                    _sw.Reset();
-                    Log = "";
-                    return string.Empty;
-                }
+                Log = "";
+                _sw.Reset();
+                File.WriteAllText(_errorDumpPath, e.Message);
 
-                br.ReadBits(blockSize, out var encodedInfo);
-                var decoding = HammingCodes.DecodeHamming(encodedInfo);
-                bw.WriteBuffer(decoding.Item1);
-                logSb.AppendLine(decoding.Item3);
-
-                var percentage = (int)(Math.Round(br.BytesRead / (float)br.FileLength, 2) * 100);
-                if (percentage > previousPercentage)
-                {
-                    previousPercentage = percentage;
-                    Log = $"Huffman-decoding... {percentage * 1}%;\tTime elapsed: {_sw.ElapsedMilliseconds / 1000f:.00}s";
-                }
+                return $"Decoding failed. Details are in the encoder error dump file";
             }
-            bw.WriteTheRestOfTheBuffer();
-            bw.StopWriting();
-            br.StopReading();
-
-            File.WriteAllText(_decoderLogPath, logSb.ToString());
-
-            _sw.Stop();
-            Log = "";
-            var res = $"Finished decoding. Decoded file:{Path.GetFileName(filepathOut)}.\n" +
-                $"\tTime elapsed: {_sw.ElapsedMilliseconds / 1000f:.00}s";
-            _sw.Reset();
-            return res;
-            //}
-            //catch (Exception e)
-            //{
-            //    Log = "";
-            //    _sw.Reset();
-            //    File.WriteAllText(_errorDumpFile, e.Message);
-
-            //    return $"Decoding failed. Details are in the encoder error dump file";
-            //}
-            //finally
-            //{
-            //    try
-            //    {
-            //        bw.StopWriting();
-            //        br.StopReading();
-            //    }
-            //    catch { }
-            //}
+            finally
+            {
+                try
+                {
+                    bw.StopWriting();
+                    br.StopReading();
+                }
+                catch { }
+            }
         }
     }
 }
--- TIK2/HammingEncoder.cs
+++ TIK2/HammingEncoder.cs
@@ -23,95 +23,95 @@ namespace TIK2
         {
             BitReader br = null;
             BitWriter bw = null;
-            //try
-        //{
-            _sw.Start();
-
-            if (!HelperMath.IsPowerOfTwo(blockSize))
-                throw new ArgumentException("block size is not a power of two");
-
-            var informationBlockSize = HammingCodes.GetInformationBlockSize(blockSize);
-
-            br = new BitReader(filepathIn);
-            bw = new BitWriter(filepathOut);
-
-            // The file format is simple.
-            // The first 2 bytes encode a byte which is the amount of overflown bits (that don't fit the informationBlockSize)
-            // The second blockSized block encodes overflown bits + some padded zeroes to the right
-            // The rest is all just encoded blocks
-
-            var len = br.FileLength;
-            var lenBits = len * 8;
-            var lenOverflow = (byte)(lenBits % informationBlockSize);
-
-            var encodedLenOverflow = HammingCodes.EncodeHamming(new BitBuffer(lenOverflow).PadRight(0, 3));
-            bw.WriteBuffer(encodedLenOverflow);
-
-            if (lenOverflow > 0)
+            try
             {
-                br.ReadBits(lenOverflow, out var overflownBuffer);
-                overflownBuffer.PadRight(0, informationBlockSize - lenOverflow);
-                bw.WriteBuffer(HammingCodes.EncodeHamming(overflownBuffer));
+                _sw.Start();
+
+                if (!HelperMath.IsPowerOfTwo(blockSize))
+                    throw new ArgumentException("block size is not a power of two");
+
+                var informationBlockSize = HammingCodes.GetInformationBlockSize(blockSize);
+
+                br = new BitReader(filepathIn);
+                bw = new BitWriter(filepathOut);
+
+                // The file format is simple.
+                // The first 2 bytes encode a byte which is the amount of overflown bits (that don't fit the informationBlockSize)
+                // The second blockSized block encodes overflown bits + some padded zeroes to the right
+                // The rest is all just encoded blocks
+
+                var len = br.FileLength;
+                var lenBits = len * 8;
+                var lenOverflow = (byte)(lenBits % informationBlockSize);
+
+                var encodedLenOverflow = HammingCodes.EncodeHamming(new BitBuffer(lenOverflow).PadRight(0, 3));
+                bw.WriteBuffer(encodedLenOverflow);
+
+                if (lenOverflow > 0)
+                {
+                    br.ReadBits(lenOverflow, out var overflownBuffer);
+                    overflownBuffer.PadRight(0, informationBlockSize - lenOverflow);
+                    bw.WriteBuffer(HammingCodes.EncodeHamming(overflownBuffer));
+                }
+
+                var previousPercentage = -1;
+
+                for (long i = lenOverflow; i < len * 8; i += informationBlockSize)
+                {
+                    if (token.IsCancellationRequested)
+                    {
+                        br.StopReading();
+                        bw.StopWriting();
+                        _sw.Reset();
+                        Log = string.Empty;
+                        return string.Empty;
+                    }
+
+                    var percentage = (int)(Math.Round(i / 8 / (float)len, 2) * 100);
+                    if (percentage > previousPercentage)
+                    {
+                        previousPercentage = percentage;
+                        Log = $"Hamming-coding... {percentage * 1}%;\tTime elapsed: {_sw.ElapsedMilliseconds / 1000f:.00}s";
+                    }
+
+                    br.ReadBits(informationBlockSize, out var infoBuffer);
+                    bw.WriteBuffer(HammingCodes.EncodeHamming(infoBuffer));
+                }
+                bw.WriteTheRestOfTheBuffer();
+                bw.StopWriting();
+                br.StopReading();
+
+                Log = "";
+                var outString = $"Finished hamming-encoding. Encoded file: {Path.GetFileName(filepathOut)}.\n" +
+                    $"\tTime elapsed: {_sw.ElapsedMilliseconds / 1000f:.00}s\n";
+                _sw.Reset();
+
+                return outString;
             }
-
-            var previousPercentage = -1;
-
-            for (long i = lenOverflow; i < len * 8; i += informationBlockSize)
+            catch (EmptyFileException e)
             {
-                if (token.IsCancellationRequested)
+                Log = "";
+                _sw.Reset();
+
+                return $"Encoding failed. Cannot encode an empty file";
+            }
+            catch (Exception e)
+            {
+                Log = "";
+                _sw.Reset();
+                File.WriteAllText(_errorDumpFile, e.Message);
+
+                return $"Encoding failed. Details are in the encoder error dump file";
+            }
+            finally
+            {
+                try
                 {
                     br.StopReading();
                     bw.StopWriting();
-                    _sw.Reset();
-                    Log = string.Empty;
-                    return string.Empty;
                 }
-
-                var percentage = (int)(Math.Round(i / 8 / (float)len, 2) * 100);
-                if (percentage > previousPercentage)
-                {
-                    previousPercentage = percentage;
-                    Log = $"Hamming-coding... {percentage * 1}%;\tTime elapsed: {_sw.ElapsedMilliseconds / 1000f:.00}s";
-                }
-
-                br.ReadBits(informationBlockSize, out var infoBuffer);
-                bw.WriteBuffer(HammingCodes.EncodeHamming(infoBuffer));
+                catch { }
             }
-            bw.WriteTheRestOfTheBuffer();
-            bw.StopWriting();
-            br.StopReading();
-
-            Log = "";
-            var outString = $"Finished hamming-encoding. Encoded file: {Path.GetFileName(filepathOut)}.\n" +
-                $"\tTime elapsed: {_sw.ElapsedMilliseconds / 1000f:.00}s\n";
-            _sw.Reset();
-
-            return outString;
-            //}
-            //catch (EmptyFileException e)
-            //{
-            //    Log = "";
-            //    _sw.Reset();
-
-            //    return $"Encoding failed. Cannot encode an empty file";
-            //}
-            //catch (Exception e)
-            //{
-            //    Log = "";
-            //    _sw.Reset();
-            //    File.WriteAllText(_errorDumpFile, e.Message);
-
-            //    return $"Encoding failed. Details are in the encoder error dump file";
-            //}
-            //finally
-            //{
-            //    try
-            //    {
-            //        br.StopReading();
-            //        bw.StopWriting();
-            //    }
-            //    catch { }
-            //}
         }
     }
 }
